4b0f2989a792c8f79c7e806bda83f5d77d752542
Estudiante <Estudiante@LAB1003-PC18>
2014-08-07 09:22:33 -0600

    Prueba

---
+++ src/ejemplo12/Ejemplo12.java
@@ -0,0 +1,13 @@
+
+package ejemplo12;
+
+import ejemplo12.vista.VentanaAplicacion;
+
+public class Ejemplo12 {
+    
+    public static void main(String[] args) {
+        VentanaAplicacion ventanaPrincipal = new VentanaAplicacion("Ejemplo 12");
+        ventanaPrincipal.init();
+    }
+    
+}
---
+++ src/ejemplo12/vista/VentanaAplicacion.java
@@ -0,0 +1,45 @@
+package ejemplo12.vista;
+
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class VentanaAplicacion extends JFrame{
+    public VentanaAplicacion(String titulo){
+        super(titulo);
+        ajustarComponentes(getContentPane());
+        setSize(new Dimension(640,480));
+        setResizable(true);
+        setLocationByPlatform(true);
+        
+        setDefaultCloseOperation(EXIT_ON_CLOSE);        
+    }
+    
+    private void ajustarComponentes(Container c){
+       //1. Ajusta el administrador de disposicion(Layout manager)
+        c.setLayout(new FlowLayout());
+        
+        //2. Coloca los componentes en el contenedor
+        c.add(new JButton("Boton 1"));
+        c.add(new JButton("Boton 2"));
+        c.add(new JButton("Boton 3"));
+        c.add(new JButton("Boton 4"));
+        c.add(new JButton("Boton 5"));
+        c.add(new JButton("Boton 6"));
+        c.add(new JButton("Boton 7"));
+        c.add(new JButton("Boton 8"));
+        
+    }
+    
+    
+    
+    
+    public void init(){
+        setVisible(true);
+        
+        System.out.println("Ventana inicializada..");
+    }
+    
+}
