b1820ee1a9210ac43c244e31eac41d42688a7430
Estudiante <Estudiante@LAB1003-PC18>
2014-08-07 10:51:20 -0600

    segunda actualizacion

--- src/ejemplo12/Ejemplo12.java
+++ src/ejemplo12/Ejemplo12.java
@@ -2,10 +2,35 @@
 package ejemplo12;
 
 import ejemplo12.vista.VentanaAplicacion;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Ejemplo12 {
     
     public static void main(String[] args) {
+        
+        //1. Ajusta el aspecto general de la aplicacion
+       try{
+           UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+           JFrame.setDefaultLookAndFeelDecorated(true);           
+       }
+       catch(ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+           System.err.println(e.getMessage());
+       }
+        SwingUtilities.invokeLater(new Runnable() {
+
+           @Override
+           public void run() {
+               iniciarInterfaz();
+           }
+       });
+        
+        System.out.println("Programa finalizado..");
+    }
+
+    private static void iniciarInterfaz() {
         VentanaAplicacion ventanaPrincipal = new VentanaAplicacion("Ejemplo 12");
         ventanaPrincipal.init();
     }
